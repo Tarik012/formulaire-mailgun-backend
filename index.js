0a0316e711ec604a22f028c609055dfb487d2e41
TARIK-MAC <administrateur@mbpdetarikmac.home>
2022-10-30 20:24:27 +0100

    change the method to get

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/send-data", async (req, res) => {
+app.get("/send-data", async (req, res) => {
   try {
     const messageData = {
       from: `${req.body.firstname} ${req.body.lastname}<${req.body.email}>`,
